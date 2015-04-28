f338c15b89e73e75441a139064dd96683399fb50
ypwhs <915505626@qq.com>
2015-04-28 23:34:41 +0800

    Initial Commit

---
+++ smarthome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smarthome
+//
+//  Created by 杨培文 on 15/4/28.
+//  Copyright (c) 2015年 杨培文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
